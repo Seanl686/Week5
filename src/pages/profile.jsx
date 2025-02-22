25687599fa3f72b926f8a7d01807f68ef7fdf002
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-02-22 17:08:01 -0500

    Refactor ProfilePage component to use useParams for dynamic profile ID display

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export const ProfilePage = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>Profile ID: {id}</p>
+    </div>
+  );
+};
   const { id } = useParams(); // Get the profile ID from URL parameters
   
   return (
