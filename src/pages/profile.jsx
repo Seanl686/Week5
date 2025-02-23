e6b97121ff4d733215bce9f9a8359feb463ccf75
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-02-22 19:52:02 -0500

    Refactor ProfilePage to use descriptive variable for useParams and improve rendering logic

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -3,12 +3,12 @@ import { useParams, Link } from 'react-router-dom';
 import './profile.css';
 
 export const ProfilePage = () => {
-  const { id } = useParams();
+  const params = useParams();
   
   return (
     <div>
-      <h1>Profile Page</h1>
-      {id ? <p>Profile ID: {id}</p> : <p>No profile ID provided</p>}
+      <h1>Profile Page {params.id}</h1>
+      {params.id ? <p>Profile ID: {params.id}</p> : <p>No profile ID provided</p>}
       <Link to="/" className="profile-button">Return to Home</Link>
     </div>
   );
